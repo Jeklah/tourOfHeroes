d1236ef4a9e9cf4dac1b8be59232b7cd86bbd1eb
Arthur Bowers <arthurbowers@gmail.com>
2019-03-13 23:52:56 +0000

    Adding import for dashboard as well as adding it to app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
